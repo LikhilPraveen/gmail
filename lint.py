d7a30eb49d5ae5115d7c9358717466adaad6ee91
LikhilPraveen <111698594+LikhilPraveen@users.noreply.github.com>
2022-09-15 03:48:51 +0530

    Update lint.py

--- lint.py
+++ lint.py
@@ -4,7 +4,8 @@ from pylint import lint
 THRESHOLD = 2
 def my_func():
   run = lint.Run(['factorial.py'], do_exit=False)
-  score = run.linter.stats["global_note"] 
+  score = Run([...], exit=False).linter.stats.global_note
+  print(score)
     
 if __name__ == '__main__':
     my_func()
