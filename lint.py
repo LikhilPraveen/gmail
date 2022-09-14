901ed58f6d4894ee62ae67b05942428ce64e4f24
LikhilPraveen <111698594+LikhilPraveen@users.noreply.github.com>
2022-09-15 05:22:13 +0530

    Update lint.py

--- lint.py
+++ lint.py
@@ -11,14 +11,14 @@ threshold = 8.0
 
 logging.info('PyLint Starting | '
              'Path: {} | '
-             'Threshold: {} '.format('factorial.py', threshold))
+             'Threshold: {} '.format('send.py', threshold))
 
 final_score = Run(['send.py'], exit=False).linter.stats.global_note
 
 logging.info(final_score)
 
 if final_score < threshold:
-
+    sys.exit(1)
     message = ('PyLint Failed | '
                'Score: {} | '
                'Threshold: {} '.format(final_score, threshold))
