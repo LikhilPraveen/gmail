1264e062ed5b82429d1d38fc0a7b4f15fe588855
LikhilPraveen <111698594+LikhilPraveen@users.noreply.github.com>
2022-09-15 04:45:47 +0530

    Update lint1.py

--- lint1.py
+++ lint1.py
@@ -8,5 +8,5 @@ score = lint.Run(['factorial.py'], exit=False).linter.stats.global_note
 
 if score < THRESHOLD:
     print("Linter failed: Score < threshold value")
-    #sys.exit(1)
+    sys.exit(1)
     
