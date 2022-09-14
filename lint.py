58b65754e4f0975f75a6009babd676dad6ff8c35
LikhilPraveen <111698594+LikhilPraveen@users.noreply.github.com>
2022-09-15 04:26:05 +0530

    Update lint.py

--- lint.py
+++ lint.py
@@ -2,32 +2,30 @@ import argparse
 import logging
 from pylint.lint import Run
 
+
 logging.getLogger().setLevel(logging.INFO)
 
-def my_func():
-  threshold = 5.0
+threshold = 5.0
 
-  results = Run(['factorial.py'], do_exit=False)
+results = Run(['factorial.py'], do_exit=False)
 
-  final_score = Run(['factorial.py'], exit=False).linter.stats.global_note
-  
-  if final_score < threshold:
+final_score = Run(['factorial.py'], exit=False).linter.stats.global_note
 
-      message = ('PyLint Failed | '
+if final_score < threshold:
+
+    message = ('PyLint Failed | '
                'Score: {} | '
                'Threshold: {} '.format(final_score, threshold))
 
-      logging.error(message)
-      raise Exception(message)
+    logging.error(message)
+    raise Exception(message)
 
-  else:
-      message = ('PyLint Passed | '
+else:
+    message = ('PyLint Passed | '
                'Score: {} | '
                'Threshold: {} '.format(final_score, threshold))
 
-      logging.info(message)
+    logging.info(message)
 
-      exit(0)
-    
-if __name__ == '__main__':
-    my_func()
+    exit(0)
+
