26bd6a5e72347fbd8d5f355ac3fbf01281ffe0cd
LikhilPraveen <111698594+LikhilPraveen@users.noreply.github.com>
2022-09-15 03:38:38 +0530

    Update lint.py

--- lint.py
+++ lint.py
@@ -2,10 +2,13 @@ import sys
 from pylint import lint
 
 THRESHOLD = 2
+def my_func():
+  run = lint.Run(['factorial.py'], do_exit=False)
+  score = run.linter.stats["global_note"] 
 
-run = lint.Run(['factorial.py'], do_exit=False)
-score = run.linter.stats["global_note"] 
-
-if score < THRESHOLD:
-    print("Linter failed: Score < threshold value")
-    sys.exit(1)
+  if score < THRESHOLD:
+     print("Linter failed: Score < threshold value")
+     sys.exit(1)
+    
+ if __name__ == '__main__':
+    my_func()
