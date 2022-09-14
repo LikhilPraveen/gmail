e705b1744a2f17dabd6a6f2d2f2bc8b46e666620
LikhilPraveen <111698594+LikhilPraveen@users.noreply.github.com>
2022-09-15 03:58:28 +0530

    Update lint.py

--- lint.py
+++ lint.py
@@ -1,11 +1,14 @@
 import sys
 from pylint import lint
+from typing import TextIO
 from pylint.lint import Run
 
 THRESHOLD = 2
 def my_func():
-  run = lint.Run(['factorial.py'], do_exit=False)
-  score = Run(['...'], exit=False).linter.stats["global_note"]
+  default_stdout = sys.stdout
+  sys.stdout = type("Dummy", (TextIO,), {"write": lambda self, data: ()})()
+  score = Run(["./src"], exit=False).linter.stats.global_note
+  sys.stdout = default_stdout
   print(score)
     
 if __name__ == '__main__':
